60683ad880086438f6e6e77271a89786717394af
alexellevu <newnapster83@gmail.com>
2021-06-16 20:52:09 +0200

    aggiunte card con immagini a sezione features

--- dist/js/app.js
+++ dist/js/app.js
@@ -13,7 +13,28 @@ var app = new Vue({
     linkNav: ['Home', 'About us', 'Feature', 'Testimonials', 'Contact US'],
     features: [{
       name: 'Customized invoices',
-      image: 'img'
+      image: '_1'
+    }, {
+      name: 'Stock Management',
+      image: '_2'
+    }, {
+      name: 'Receivable & Payables',
+      image: '_3'
+    }, {
+      name: 'Manage Buying',
+      image: '_4'
+    }, {
+      name: 'Powerful & Secure',
+      image: '_5'
+    }, {
+      name: 'Fastest Returnig Filing',
+      image: '_6'
+    }, {
+      name: 'Monthly Detailed',
+      image: '_7'
+    }, {
+      name: 'Product Management',
+      image: '_8'
     }]
   },
   methods: {}
--- src/app.js
+++ src/app.js
@@ -3,7 +3,14 @@ const app = new Vue({
     data:{
         linkNav:['Home','About us','Feature','Testimonials','Contact US'],
         features:[
-            {'Customized invoices','img'}
+            {name:'Customized invoices',image:'_1'},
+            {name:'Stock Management',image:'_2'},
+            {name:'Receivable & Payables',image:'_3'},
+            {name:'Manage Buying',image:'_4'},
+            {name:'Powerful & Secure',image:'_5'},
+            {name:'Fastest Returnig Filing',image:'_6'},
+            {name:'Monthly Detailed',image:'_7'},
+            {name:'Product Management',image:'_8'}
         ]
     },
 
